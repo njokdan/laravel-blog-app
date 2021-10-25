110e3f9af2424fe1651e98dc83fffcd68d024556
Daniel Njoku <njokdan@gmail.com>
2021-10-25 15:24:57 +0100

    update commit

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Str;
-//$DATABASE_URL = parse_url('postgres://shtkgsgmngtzah:31d751bf7587ee0626d006a8804c9318b693010b984788197442518451b3c7c5@ec2-34-196-34-142.compute-1.amazonaws.com:5432/d8ts75dsh6fkq6');
+
 return [
 
     /*
@@ -65,15 +65,17 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => $DATABASE_URL["host"],
-            'port' => $DATABASE_URL["port"],
-            'database' => ltrim($DATABASE_URL["path"], "/"),
-            'username' => $DATABASE_URL["user"],
-            'password' => $DATABASE_URL["pass"],
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
+            'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'require',
+            'sslmode' => 'prefer',
         ],
 
         'sqlsrv' => [
